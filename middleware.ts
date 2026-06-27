3c29cc2045f4af12fdfa18e59bd65b2e7ad53620
lflspimenta <luisfpimenta@gmail.com>
2026-06-27 20:06:25 +0100

    Organizar edições disponíveis primeiro

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,17 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/imobiliario") {
+    return NextResponse.rewrite(
+      new URL("/agenda-imobiliario-landing.html", request.url)
+    );
+  }
+
   return NextResponse.rewrite(
     new URL("/agenda-ativa-homepage-final.html", request.url)
   );
 }
 
 export const config = {
-  matcher: ["/"]
+  matcher: ["/", "/imobiliario"]
 };
